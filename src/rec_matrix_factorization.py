cb0d8ab5ab7c383f0c82c0a9ad391b4b70a31d61
HGX Server <phananhduc138@gmail.com>
2024-04-13 19:34:43 +0900

    add pydoc comment to recommendation code

--- src/rec_matrix_factorization.py
+++ src/rec_matrix_factorization.py
@@ -1,35 +1,104 @@
-import rootutils
+"""Matrix Factorization model for recommendation
+
+This script trains a matrix factorization model for recommendation using the MovieLens dataset.
+The model is trained using PyTorch Lightning.
+The configuration is done using Hydra (data module, model, callbacks, trainer).
+The data module downloads the MovieLens dataset, splits it into train, val, test sets.
+The model is a matrix factorization model with a score network.
+The model is trained and tested using MSE loss and evaluated using MAE metric.
+The trainer is configured to use a GPU if available and to use a checkpoint callback to save the best model.
+"""
 import logging
-import hydra
-from omegaconf import DictConfig, OmegaConf
-import torch
-from torch.utils.data import DataLoader, Dataset
-import lightning as L
-import urllib.request as request
-from pathlib import Path
 import os
+from pathlib import Path
 from zipfile import ZipFile
-import pandas as pd
-from sklearn.model_selection import train_test_split
+
+import hydra
+import lightning as L
 import numpy as np
+import pandas as pd
+import rootutils
+import torch
 import torchmetrics as tm
+from omegaconf import DictConfig, OmegaConf
+from sklearn.model_selection import train_test_split
+from torch.utils.data import DataLoader, Dataset
+from tqdm import tqdm
+from urllib import request
 
 
 logging.basicConfig(level=logging.INFO)
+# Set up the root path of the project.
 # import from src after this line
-root_path = rootutils.setup_root(
+ROOT_PATH = rootutils.setup_root(
     __file__, indicator=".project_root", pythonpath=True)
-config_path = str(root_path / "config")
+CONFIG_PATH = str(ROOT_PATH / "config")
+
+
+class DownloadHook:
+    """Wraps tqdm instance.
+    Adapt from: https://gist.github.com/leimao/37ff6e990b3226c2c9670a2cd1e4a6f5
+    Don't forget to close() or __exit__()
+    the tqdm instance once you're done with it (easiest using `with` syntax).
+    Example
+    -------
+    >>> with tqdm(...) as t:
+    ...     reporthook = my_hook(t)
+    ...     urllib.urlretrieve(..., reporthook=reporthook)
+    """
+
+    def __init__(self, t: tqdm):
+        self.t = t
+        self.last_b = 0
+
+    def __call__(self, b=1, bsize=1, tsize=None):
+        """
+        b  : int, optional
+            Number of blocks transferred so far [default: 1].
+        bsize  : int, optional
+            Size of each block (in tqdm units) [default: 1].
+        tsize  : int, optional
+            Total size (in tqdm units). If [default: None] remains unchanged.
+        """
+        if tsize is not None:
+            self.t.total = tsize
+        self.t.update((b - self.last_b) * bsize)
+        self.last_b = b
 
 
 class MovieLensDataset(Dataset):
+    """MovieLens dataset
+    """
+
     def __init__(self, df):
+        """Initialize the dataset
+
+        Args:
+            df (pd.DataFrame): dataframe with columns user, item, rating, timestamp
+        """
         self.df = df
 
     def __len__(self):
+        """Return the length of the dataset
+
+        Returns:
+            int: length of the dataset
+        """
         return len(self.df)
 
     def __getitem__(self, idx):
+        """Get the item at index idx
+
+        Args:
+            idx (int): index
+
+        Returns:
+            dict: dictionary with keys user, item, rating
+
+        Note:
+            user and item are 0-indexed
+            rating is normalized to [0, 1]
+        """
         values = self.df.iloc[idx].values
         return dict(
             user=int(values[0]) - 1, item=int(values[1]) - 1, rating=np.float32(values[2] / 5.0)
@@ -37,20 +106,35 @@ class MovieLensDataset(Dataset):
 
 
 class MovieLensDataModule(L.LightningDataModule):
+    """MovieLens lightning data module"""
+
     def __init__(
         self,
         root_path,
         data_relative_path,
-        data_url="https://files.grouplens.org/datasets/movielens/ml-100k.zip",
+        data_url,
         batch_size=64,
         num_workers=4,
     ):
+        """Initialize the data module
+
+        Args:
+            root_path (_type_): the root path of the data (containing the zip file)
+            data_relative_path (_type_): the relative path of the data file within the root path
+            data_url (str, optional): url to download.
+            batch_size (int, optional): batch size for dataloaders. Defaults to 64.
+            num_workers (int, optional): number of workers for dataloaders. Defaults to 4.
+        """
         super().__init__()
         self.save_hyperparameters()
         self.train_df, self.val_df, self.test_df = None, None, None
         self.train_dataset, self.val_dataset, self.test_dataset = None, None, None
 
     def prepare_data(self):
+        """Download and unzip the data if not already downloaded and unzipped
+        Zip file is downloaded from data_url and unzipped to root_path
+        Check if the data file already exists in the data relative path an unzip if not
+        """
         # download zip data from data_url to root_path
         root_path = Path(self.hparams.root_path)
         zip_file_name = os.path.basename(self.hparams.data_url)
@@ -59,7 +143,9 @@ class MovieLensDataModule(L.LightningDataModule):
             logging.info(f"File {zip_file_path} already exists")
         else:
             logging.info(f"Downloading {self.hparams.data_url} to {zip_file_path}")
-            request.urlretrieve(self.hparams.data_url, zip_file_path, reporthook=self.download_hook)
+
+            with tqdm(unit='B', unit_scale=True, unit_divisor=1024, miniters=1, desc=str(zip_file_path)) as t:
+                request.urlretrieve(self.hparams.data_url, zip_file_path, reporthook=DownloadHook(t), data=None)
             logging.info("Data downloaded")
 
         # unzip the file
@@ -71,10 +157,11 @@ class MovieLensDataModule(L.LightningDataModule):
         else:
             logging.info(f"File {zip_file_path} already unzipped")
 
-    def download_hook(self, block_num, block_size, total_size):
-        logging.info(f"Downloading {block_num * block_size / total_size * 100:.2f}%")
-
     def setup(self, stage: str):
+        """Setup the data module
+        Split the data frame into train, val, test
+        Create the train, val, test datasets
+        """
         # read into dataframe
         root_path = Path(self.hparams.root_path)
         data_path = root_path / self.hparams.data_relative_path
@@ -107,7 +194,27 @@ class MovieLensDataModule(L.LightningDataModule):
 
 
 class MatrixFactorization(L.LightningModule):
-    def __init__(self, num_users, num_items, score_net, optimizer, n_hidden=100, embedding_dim=32):
+    """Matrix Factorization model"""
+
+    def __init__(
+        self,
+        num_users: int,
+        num_items: int,
+        score_net: torch.nn.Module,
+        optimizer: torch.optim.Optimizer,
+        n_hidden=100,
+        embedding_dim=32
+    ):
+        """Initialize the model
+
+        Args:
+            num_users (int): number of users
+            num_items (int): number of items
+            score_net (torch.nn.Module): score network
+            optimizer: optimizer
+            n_hidden (int, optional): number of hidden units. Defaults to 100.
+            embedding_dim (int, optional): embedding dimension. Defaults to 32.
+        """
         super().__init__()
         self.save_hyperparameters(ignore=["score_net"])
         self.user_embeddings = torch.nn.Embedding(num_users, embedding_dim)
@@ -125,16 +232,31 @@ class MatrixFactorization(L.LightningModule):
         self.test_mae = tm.MeanAbsoluteError()
 
     def init_weights(self, m):
+        """Initialize the weights of the score model layer"""
         if isinstance(m, torch.nn.Linear):
             torch.nn.init.xavier_uniform(m.weight)
             m.bias.data.fill_(0.01)
 
     def initialize_parameters(self):
+        """Initialize the parameters of the model"""
         torch.nn.init.normal_(self.user_embeddings.weight, std=0.01)
         torch.nn.init.normal_(self.item_embeddings.weight, std=0.01)
         self.score_net.apply(self.init_weights)
 
     def forward(self, user, item):
+        """Forward pass
+
+        Args:
+            user (torch.Tensor): user index, size [B]
+            item (torch.Tensor): item index, size [B]
+
+        Returns:
+            torch.Tensor: score prediction, size [B]
+
+        Note:
+            B is the batch size
+            user and item are 0-indexed
+        """
         user_embedding = self.user_embeddings(user)
         item_embedding = self.item_embeddings(item)
         # x_term = user_embedding * item_embedding
@@ -143,6 +265,16 @@ class MatrixFactorization(L.LightningModule):
         return out.squeeze(1)
 
     def step(self, batch, loss_metric, mae_metric):
+        """Step function for training, validation and testing
+
+        Args:
+            batch (dict): batch with keys user, item, rating
+            loss_metric (torchmetrics.Metric): loss metric
+            mae_metric (torchmetrics.Metric): mae metric
+
+        Returns:
+            tuple: loss, prediction, target rating
+        """
         user, item, rating = batch['user'], batch['item'], batch['rating']
         prediction = self(user, item)
         loss = torch.nn.functional.mse_loss(prediction, rating)
@@ -150,38 +282,88 @@ class MatrixFactorization(L.LightningModule):
         mae_metric(prediction, rating)
         return loss, prediction, rating
 
-    def on_train_start(self) -> None:
+    def on_train_start(self):
+        """Reset the validation loss and mae metrics in case the test run initializes them"""
         self.val_mae.reset()
         self.val_mae_best.reset()
 
     def on_validation_epoch_end(self):
+        """Update the best validation mae metric after each validation epoch"""
         mae = self.val_mae.compute()
         self.val_mae_best(mae)
-        self.log("val/mae_best", self.val_mae_best.compute(), sync_dist=True, prog_bar=True)
+        self.log("val/mae_best", self.val_mae_best.compute(),
+                 sync_dist=True, prog_bar=True)
 
     def training_step(self, batch, batch_idx):
+        """Training step
+
+        Args:
+            batch (dict): batch with keys user, item, rating
+            batch_idx (int): batch index
+
+        Returns:
+            loss (torch.Tensor): mse loss
+
+        Note:
+            The training loss and mae are computed and logged
+        """
         loss, prediction, rating = self.step(batch, self.train_loss, self.train_mae)
         self.log("train/loss", self.train_loss, on_step=False, on_epoch=True, prog_bar=True)
         self.log("train/mae", self.train_mae, on_step=False, on_epoch=True, prog_bar=True)
         return loss
 
     def validation_step(self, batch, batch_idx):
+        """Validation step
+
+        Args:
+            batch (dict): batch with keys user, item, rating
+            batch_idx (int): batch index
+
+        Note:
+            The validation loss and mae are computed and logged
+        """
         loss, prediction, rating = self.step(batch, self.val_loss, self.val_mae)
         self.log("val/loss", self.val_loss, on_step=False, on_epoch=True, prog_bar=True, sync_dist=True)
         self.log("val/mae", self.val_mae, on_step=False, on_epoch=True, prog_bar=True, sync_dist=True)
 
     def test_step(self, batch, batch_idx):
-        loss, prediction, rating = self.step(batch, self.test_loss, self.test_mae)
+        """Test step
+
+        Args:
+            batch (dict): batch with keys user, item, rating
+            batch_idx (int): batch index
+
+        Note:
+            The test loss and mae are computed and logged
+        """
+        loss, prediction, rating = self.step(
+            batch, self.test_loss, self.test_mae)
         self.log("test/loss", self.test_loss, on_step=False, on_epoch=True, prog_bar=True, sync_dist=True)
         self.log("test/mae", self.test_mae, on_step=False, on_epoch=True, prog_bar=True, sync_dist=True)
 
     def configure_optimizers(self):
+        """Configure the optimizer
+
+        Note:
+            The optimizer is patially configured from the hparams
+            At this point, the optimizer needs to be fully configured
+            using the parameters of the model
+        """
         optimizer = self.hparams.optimizer(self.parameters())
         return optimizer
 
 
-@hydra.main(version_base=None, config_path=config_path, config_name="rec")
+@hydra.main(version_base=None, config_path=CONFIG_PATH, config_name="rec")
 def main(cfg: DictConfig) -> None:
+    """Main function to train and test the model
+
+    Args:
+        cfg (DictConfig): configuration object from config file
+        cfg.data_module (DictConfig): data module configuration
+        cfg.model (DictConfig): model configuration
+        cfg.callbacks (List[DictConfig]): list of callback configurations
+        cfg.trainer (DictConfig): trainer configuration
+    """
     print(OmegaConf.to_yaml(cfg))
     L.seed_everything(cfg.seed)
 
